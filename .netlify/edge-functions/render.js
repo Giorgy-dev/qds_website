dd61c551f32d5bc7f9d92ec56b71a9457c8a3724
Giorgy-dev <marco.gorgetti.2@gmail.com>
2025-01-27 12:08:45 +0100

    fixes

--- .netlify/edge-functions/render.js
+++ .netlify/edge-functions/render.js
@@ -1314,7 +1314,7 @@ var init__ = __esm({
   ".svelte-kit/output/server/nodes/0.js"() {
     index = 0;
     component = async () => component_cache ?? (component_cache = (await Promise.resolve().then(() => (init_layout_svelte(), layout_svelte_exports))).default);
-    imports = ["_app/immutable/nodes/0.DSiAwOYk.js", "_app/immutable/chunks/scheduler.Bu15-wVR.js", "_app/immutable/chunks/index.O99BFJ2P.js", "_app/immutable/chunks/Section.VqIItiC4.js", "_app/immutable/chunks/Typewriter.oItSYdui.js", "_app/immutable/chunks/stores.DX2LahA5.js", "_app/immutable/chunks/entry.CsdPvztV.js"];
+    imports = ["_app/immutable/nodes/0.DfyxRKgU.js", "_app/immutable/chunks/scheduler.Bu15-wVR.js", "_app/immutable/chunks/index.O99BFJ2P.js", "_app/immutable/chunks/Section.VqIItiC4.js", "_app/immutable/chunks/Typewriter.oItSYdui.js", "_app/immutable/chunks/stores.0Py7OsEy.js", "_app/immutable/chunks/entry.UH5c8Hfg.js"];
     stylesheets = ["_app/immutable/assets/0.LryB5bzY.css", "_app/immutable/assets/Section._ZdJInvP.css"];
     fonts = [];
   }
@@ -1353,7 +1353,7 @@ var init__2 = __esm({
   ".svelte-kit/output/server/nodes/1.js"() {
     index2 = 1;
     component2 = async () => component_cache2 ?? (component_cache2 = (await Promise.resolve().then(() => (init_error_svelte(), error_svelte_exports))).default);
-    imports2 = ["_app/immutable/nodes/1.Drz-TsDf.js", "_app/immutable/chunks/scheduler.Bu15-wVR.js", "_app/immutable/chunks/index.O99BFJ2P.js", "_app/immutable/chunks/stores.DX2LahA5.js", "_app/immutable/chunks/entry.CsdPvztV.js"];
+    imports2 = ["_app/immutable/nodes/1.Do7YSIwT.js", "_app/immutable/chunks/scheduler.Bu15-wVR.js", "_app/immutable/chunks/index.O99BFJ2P.js", "_app/immutable/chunks/stores.0Py7OsEy.js", "_app/immutable/chunks/entry.UH5c8Hfg.js"];
     stylesheets2 = [];
     fonts2 = [];
   }
@@ -2242,7 +2242,7 @@ var options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1qlf6lw"
+  version_hash: "1lm6b4"
 };
 async function get_hooks() {
   return {};
@@ -5227,7 +5227,7 @@ var manifest = (() => {
     assets: /* @__PURE__ */ new Set([".DS_Store", "assets/.DS_Store", "assets/fonts/.DS_Store", "assets/fonts/FFF Acid Grotesk Bold.eot", "assets/fonts/FFF Acid Grotesk Bold.otf", "assets/fonts/FFF Acid Grotesk Bold.svg", "assets/fonts/FFF Acid Grotesk Bold.woff", "assets/fonts/FFF Acid Grotesk Bold.woff2", "assets/fonts/FFFAcidGroteskVariable.ttf", "assets/graphics/chirale.svg", "assets/graphics/connection.svg", "assets/graphics/contrasto.svg", "assets/graphics/determinazione.svg", "assets/graphics/espandi.svg", "assets/graphics/expand.svg", "assets/graphics/expand_in.svg", "assets/graphics/expand_out.svg", "assets/graphics/expand_us.svg", "assets/graphics/pen.svg", "assets/graphics/pulizia.svg", "assets/graphics/sfondo.svg", "assets/img/stock_portrait.jpg", "assets/img/stock_portrait.webp", "assets/logo/logo.svg", "assets/logo/logo_extended.svg", "assets/logo/logo_grid.svg", "assets/logo/logo_partial.svg", "assets/logo/logo_partial_white.svg", "assets/logo/text.svg", "assets/logo/text_extended.svg", "assets/video/.DS_Store", "assets/video/branding.mp4", "assets/video/branding_stock.mp4", "assets/video/fotografia.mp4", "assets/video/grafica.mp4", "assets/video/sounddesign.mp4", "assets/video/sounddesign2.mp4", "assets/video/uiux.mp4", "favicon.png"]),
     mimeTypes: { ".otf": "font/otf", ".svg": "image/svg+xml", ".woff": "font/woff", ".woff2": "font/woff2", ".ttf": "font/ttf", ".jpg": "image/jpeg", ".webp": "image/webp", ".mp4": "video/mp4", ".png": "image/png" },
     _: {
-      client: { "start": "_app/immutable/entry/start.4D6NwjgB.js", "app": "_app/immutable/entry/app.znb67TI6.js", "imports": ["_app/immutable/entry/start.4D6NwjgB.js", "_app/immutable/chunks/entry.CsdPvztV.js", "_app/immutable/chunks/scheduler.Bu15-wVR.js", "_app/immutable/entry/app.znb67TI6.js", "_app/immutable/chunks/scheduler.Bu15-wVR.js", "_app/immutable/chunks/index.O99BFJ2P.js"], "stylesheets": [], "fonts": [], "uses_env_dynamic_public": false },
+      client: { "start": "_app/immutable/entry/start.CiS95esO.js", "app": "_app/immutable/entry/app.CG4FMODF.js", "imports": ["_app/immutable/entry/start.CiS95esO.js", "_app/immutable/chunks/entry.UH5c8Hfg.js", "_app/immutable/chunks/scheduler.Bu15-wVR.js", "_app/immutable/entry/app.CG4FMODF.js", "_app/immutable/chunks/scheduler.Bu15-wVR.js", "_app/immutable/chunks/index.O99BFJ2P.js"], "stylesheets": [], "fonts": [], "uses_env_dynamic_public": false },
       nodes: [
         __memo(() => Promise.resolve().then(() => (init__(), __exports))),
         __memo(() => Promise.resolve().then(() => (init__2(), __exports2))),
